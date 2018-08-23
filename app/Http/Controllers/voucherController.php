f50e1a5c69f3a718e5f9f7276cce9a268cbf7818
Suleiman Mamman A <suleimamman@gmail.com>
2018-08-23 10:52:07 +0100

    VoucherController Created

---
+++ app/Http/Controllers/voucherController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\InsertRecipient;
+use App\Models\GetRecipient;
+use App\Models\InsertSpecialOffer;
+use App\Models\GetSpecialOffer;
+use App\Models\InsertVoucherCode;
+use App\Models\GetVoucherCode;
+use Validator;
+
+class voucherController extends Controller
+{
+    //
+}
